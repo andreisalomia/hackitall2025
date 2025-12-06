bd3f53af0281bb1100972527253630894db66e6e
andreisalomia <andreisalomia100@gmail.com>
2025-12-06 14:46:12 +0200

    chenar tape recorder

--- frontend/src/TapeRecorder/TapeRecorder.jsx
+++ frontend/src/TapeRecorder/TapeRecorder.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TapeRecorder.css';
 
-// Importăm imaginile tale
-import tapeBody from './tape2_pixel_modif.png'; 
-import singleReel from './rola_pixel.png'; 
+import tapeBody from './tape2_pixel_modif.png';
+import singleReel from './rola_pixel.png';
 
-const TapeRecorder = ({ onClick }) => {
+export default function MatrixTapeRecorder() {
+  const [isRecording, setIsRecording] = useState(false);
+
   return (
-    // Containerul principal
-    <div className="tape-wrapper" onClick={onClick}>
-      
-      {/* 1. Imaginea de fundal (Casetofonul întreg) */}
-      <img 
-        src={tapeBody} 
-        alt="Tape Recorder Base" 
-        className="tape-bg" 
-      />
+    <div className="matrix-wrapper">
 
-      {/* 2. Rola Stângă (Animată) */}
-      <img 
-        src={singleReel} 
-        alt="Spinning Reel Left" 
-        className="reel reel-left" 
-      />
+      <div className="matrix-frame">
 
-      {/* 3. Rola Dreaptă (Animată) */}
-      <img 
-        src={singleReel} 
-        alt="Spinning Reel Right" 
-        className="reel reel-right"
-      />
-      <div className="blink-square"></div>
-      
+        {/* ZONA PRINCIPALĂ ORIZONTALĂ */}
+        <div className="matrix-layout">
+
+          {/* CASETOFON */}
+          <div className="tape-wrapper">
+            <img src={tapeBody} alt="Tape Recorder Base" className="tape-bg" />
+
+            {/* RULE ALINIATE CORECT */}
+            <img
+              src={singleReel}
+              alt="Left Reel"
+              className={`reel reel-left ${isRecording ? 'spin' : ''}`}
+            />
+
+            <img
+              src={singleReel}
+              alt="Right Reel"
+              className={`reel reel-right ${isRecording ? 'spin' : ''}`}
+            />
+
+            {isRecording && <div className="blink-square"></div>}
+          </div>
+
+          {/* BUTOANE */}
+          <div className="matrix-buttons">
+            <button className="mx-btn record" onClick={() => setIsRecording(true)}>
+              RECORD
+            </button>
+
+            <button className="mx-btn stop" onClick={() => setIsRecording(false)}>
+              STOP
+            </button>
+
+            <button className="mx-btn summary">
+              REZUMATUL ZILEI
+            </button>
+
+            <button className="mx-btn todo">
+              TODO'S
+            </button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
-};
-
-export default TapeRecorder;
+}
